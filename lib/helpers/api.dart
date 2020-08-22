84ab0220e5fce22cc3117a2f279d9f403f109e52
mariovzc <mario.vizcainos@gmail.com>
2020-08-22 00:13:34 -0500

    Generic api class

---
+++ lib/helpers/api.dart
@@ -0,0 +1,29 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import 'package:pokedex/models/pokemon.dart';
+import 'package:pokedex/models/pokemons.dart';
+
+class API {
+  static const BASEURL = "https://pokeapi.co/api/v2/pokemon";
+
+  static Future<Pokemons> getAll({limit = 20, offset = 0}) async {
+    final url = "{BASEURL}?limit={limit}&offset={offset}";
+    final req = await http.get(url);
+
+    if (req.statusCode != 200) throw Exception(req.statusCode.toString());
+
+    final res = convert.jsonDecode(req.body);
+
+    return Pokemons.fromJson(res);
+  }
+
+  static Future<Pokemon> getItem(String pokeUrl) async {
+    final req = await http.get(pokeUrl);
+
+    if (req.statusCode != 200) throw Exception(req.statusCode.toString());
+
+    final res = convert.jsonDecode(req.body);
+
+    return Pokemon.fromJson(res);
+  }
+}
