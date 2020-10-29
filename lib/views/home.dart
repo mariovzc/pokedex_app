88874d948571c8ce1071d56cc2cff1de4eab445a
Maximiliano Valeta <mvaleta@altocolombia.com.co>
2020-10-29 15:25:51 -0500

    ADD : View list or grid

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,16 +3,34 @@ import 'package:provider/provider.dart';
 import 'package:pokedex/widgets/list_items.dart';
 import 'package:pokedex/providers/pokemon_list.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
 
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int listOrGrid = 2;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: Image.network(
-            "https://img.icons8.com/color/48/000000/pokeball-2.png"),
+          "https://img.icons8.com/color/48/000000/pokeball-2.png",
+        ),
         title: const Text('PokeDex'),
+        actions: [
+          IconButton(
+              icon: Icon(
+                listOrGrid == 1 ? Icons.list : Icons.grid_on,
+                color: Colors.white,
+                size: 30,
+              ),
+              onPressed: () {
+                setState(() => listOrGrid = listOrGrid == 2 ? 1 : 2);
+              })
+        ],
       ),
       body: SafeArea(
         child: FutureBuilder(
@@ -29,7 +47,7 @@ class Home extends StatelessWidget {
                 );
                 break;
               default:
-                return ListItems();
+                return ListItems(listOrGrid);
             }
           },
         ),
